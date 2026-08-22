b59deb1c80dbd24fd3760aba0d9f26f08c2c6684
ikosoft <fullstackwebdevlopment3030@gmail.com>
2026-08-22 10:15:38 +0100

    updating some stuff

--- krooztvus-web/app/sitemap.js
+++
@@ -1,8 +0,0 @@
-const routes = ["", "/pricing", "/channels-vod", "/setup-installation", "/about", "/blog", "/faq", "/contact", "/privacy", "/terms"];
-
-export default function sitemap() {
-  return routes.map((route) => ({
-    url: `https://krooztvus.us${route}`,
-    lastModified: new Date(),
-  }));
-}
